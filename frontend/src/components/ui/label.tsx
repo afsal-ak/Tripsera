a6c0cf0d53e6f20d79b7f2af9fc39c7e85230fa6
Afsal-ak <afsalak070@gmail.com>
2025-10-29 16:16:57 +0530

    created new Label

--- frontend/src/components/ui/label.tsx
+++ frontend/src/components/ui/label.tsx
@@ -1,39 +1,16 @@
-// import * as React from 'react';
-// import * as LabelPrimitive from '@radix-ui/react-label';
-// import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from 'react'
 
-// import { cn } from '@/lib/utils';
+interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  children: React.ReactNode
+}
 
-// const labelVariants = cva(
-//   'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-// );
-
-// const Label = React.forwardRef<
-//   React.ElementRef<typeof LabelPrimitive.Root>,
-//   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & VariantProps<typeof labelVariants>
-// >(({ className, ...props }, ref) => (
-//   <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
-// ));
-// Label.displayName = LabelPrimitive.Root.displayName;
-
-// export { Label };
-
-import * as React from 'react';
-import * as LabelPrimitive from '@radix-ui/react-label';
-import { cva, type VariantProps } from 'class-variance-authority';
-import { cn } from '@/lib/utils';
-
-const labelVariants = cva(
-  'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-);
-
-const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
-));
-
-Label.displayName = LabelPrimitive.Root.displayName;
-
-export { Label };
+export const Label: React.FC<LabelProps> = ({ children, className = '', ...props }) => {
+  return (
+    <label
+      className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${className}`}
+      {...props}
+    >
+      {children}
+    </label>
+  )
+}
