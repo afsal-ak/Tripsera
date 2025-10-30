050eebc085bb6cac1de8feb66c0198d2454cba29
Afsal-ak <afsalak070@gmail.com>
2025-10-30 14:03:05 +0530

    updated socket connection url

--- frontend/src/sockets/socket.ts
+++ frontend/src/sockets/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = import.meta.env.VITE_API_BACKEND_URL
+const SOCKET_URL = import.meta.env.VITE_API_BASE_URL
  || 'http://localhost:4001';
 
 export const socket: Socket = io(SOCKET_URL, {
